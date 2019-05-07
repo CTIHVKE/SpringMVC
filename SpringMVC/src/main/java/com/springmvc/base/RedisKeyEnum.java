da909201cd86070cc4ab3d762c91512feb200b5c
ihvke <ctihvke@outlook.com>
2019-05-08 00:19:14 +0800

    Redis 键枚举

---
+++ SpringMVC/src/main/java/com/springmvc/base/RedisKeyEnum.java
@@ -0,0 +1,15 @@
+package com.springmvc.base;
+
+public enum  RedisKeyEnum {
+//    用记表数据列表
+    SysUserList,
+//    测试列表
+    List,
+//
+    SetKey,
+//    登录名
+    LoginName,
+//    hash 表字
+    Hash
+
+}
